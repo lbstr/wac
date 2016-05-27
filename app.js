1efe3cb7d1dc4fcbe9fd755085156678ddf3f708
Jared Barnett <jmwbarnett@gmail.com>
2016-05-27 15:53:18 -0600

    adjusting static file config

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var app = express();
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use('/', router);
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // TODO: remove this and use templatecache
-app.use(express.static('app'));
+app.use(express.static(path.join(__dirname, 'app')));
 
-app.listen(8081);
+app.listen(process.env.PORT || 8081);
