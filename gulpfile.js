a392cef2883070f813b6a090d57f059a62133583
Jared Barnett <jmwbarnett@gmail.com>
2016-06-15 15:37:07 -0600

    minifying all js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var del = require('del');
 var addStream = require('add-stream');
 var plugins = require('gulp-load-plugins')();
 
-gulp.task('default', ['js', 'vendor-js']);
+gulp.task('default', ['js']);
 
 gulp.task('js', function() {
   var processTemplates = function() {
@@ -14,20 +14,14 @@ gulp.task('js', function() {
   };
 
   return gulp.src([
+      'bower_components/angular/angular.js',
       'client/**/*module*.js',
       'client/**/*.js',
     ])
     .pipe(addStream.obj(processTemplates()))
-    .pipe(plugins.concat('all.js'))
+    .pipe(plugins.concat('all.min.js'))
+    .pipe(plugins.uglify())
     .pipe(gulp.dest('public/js'));
-});
-
-gulp.task('vendor-js', function(){
-  return gulp.src([
-      'bower_components/angular/angular*.js?(.map)'
-    ])
-    .pipe(gulp.dest('public/js'));
-
 });
 
 gulp.task('clean', function(){
